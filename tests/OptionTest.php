1959e3063b58068ffb56b8076e81d5835293d2bd
Ciaran <cslemon96@gmail.com>
2025-11-23 00:19:33 +0200

    chore(tests): setup test scaffolds

---
+++ tests/OptionTest.php
@@ -0,0 +1,6 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class OptionTest extends TestCase {
+}
---
+++ tests/ResultTest.php
@@ -0,0 +1,6 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ResultTest extends TestCase {
+}
